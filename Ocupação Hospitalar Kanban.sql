1cd0d6ed08f419b5f37cdbbada7da585290a2763
Marcelo Melo <marcelosdm@gmail.com>
2017-12-13 14:57:38 -0200

    add filters

--- Ocupação Hospitalar Kanban.sql
+++ Ocupação Hospitalar Kanban.sql
@@ -2,6 +2,7 @@ select distinct
   a.dt_entrada,
   nvl(trunc(sysdate) - trunc(a.dt_entrada), 0)  qt_dia_permanencia,
   obter_qt_dia_internacao_sus(d.cd_procedimento, d.ie_origem_proced)dias_sus,
+  obter_se_maior_limite_sus(a.nr_atendimento, obter_qt_dia_internacao_sus(d.cd_procedimento, d.ie_origem_proced))kanban_cru,
   case obter_se_maior_limite_sus(a.nr_atendimento, obter_qt_dia_internacao_sus(d.cd_procedimento, d.ie_origem_proced))
   WHEN 'I' then 'AMARELO'
   WHEN 'S' then 'VERMELHO'
@@ -27,5 +28,6 @@ where
   AND a.nr_atendimento = c.nr_atendimento(+)
   AND c.CD_PROCEDIMENTO_SOLIC = d.CD_PROCEDIMENTO(+)
   AND (a.CD_SETOR_ATUAL =:cd_setor_atual or NVL(:cd_setor_atual ,0)= 0)
+  AND (obter_se_maior_limite_sus(a.nr_atendimento, obter_qt_dia_internacao_sus(d.cd_procedimento, d.ie_origem_proced)) =:kanban OR NVL(:kanban,0) = 'todos')
 order by ds_setor_atendimento, cd_unidade, 1,4,
 a.nm_paciente
